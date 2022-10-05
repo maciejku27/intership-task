e6eed70dc995cc4f6f7bbf4494e0716ea114fdcf
maciejku27 <kuczynski.maciej.nh@gmail.com>
2022-10-05 15:33:45 +0200

    design changes

--- src/components/atoms/Search/SearchBar.vue
+++ src/components/atoms/Search/SearchBar.vue
@@ -1,37 +1,79 @@
 <template>
   <a-space direction="vertical" :size="16">
-    <form @submit.prevent="getLocation">
-      <input
-        type="text"
-        v-model="searchLocation"
-        placeholder="Search location..." />
-      <button>Search</button>
-    </form>
     <a-input-search
       type="text"
-      v-model:search="searchLocation"
+      v-model:value="searchLocation"
       placeholder="Search location..."
       enter-button="Search"
       size="large"
       @search="getLocation" />
-    <a-card v-if="info != null">
-      <a-space align="baseline" :size="40">
-        <a-typography-title>{{ info }}</a-typography-title>
-        <a-typography-title :level="3"> {{ info2 }}°C </a-typography-title>
-      </a-space>
-      <div>
-        <a-space :size="30">
-          <a-typography-text>Pressure:{{ info5 }}hPa </a-typography-text>
-          <a-typography-text>Visibility:{{ info6 }}m </a-typography-text>
-        </a-space>
-      </div>
-      <div>
-        <a-space :size="30">
-          <a-typography-text>Wind: {{ info3 }}m/s </a-typography-text>
-          <a-typography-text>Humidity: {{ info4 }}% </a-typography-text>
-        </a-space>
-      </div>
-    </a-card>
+    <a-tabs v-model:activeKey="activeKey">
+      <a-tab-pane key="1" tab="Weather">
+        <a-card v-if="name != null">
+          <div>
+            <a-typography-title :level="3">
+              {{ name }},{{ country }}
+            </a-typography-title>
+          </div>
+          <div>
+            <a-typography-title> {{ temp }}°C </a-typography-title>
+          </div>
+          <div>
+            <a-space :size="30">
+              <a-statistic title="Pressure:" :value="pressure" suffix="hPa" />
+              <a-statistic title="Visibility:" :value="visibility" suffix="m" />
+            </a-space>
+          </div>
+          <div>
+            <a-space :size="45">
+              <a-statistic title="Wind:" :value="windSpeed" suffix="m/s" />
+              <a-statistic title="Humidity:" :value="humidity" suffix="%" />
+            </a-space>
+          </div>
+        </a-card>
+        <a-card v-else>
+          <a-typography-title :level="4">
+            Write a name of the country you want to check weather of in the
+            search bar
+          </a-typography-title>
+        </a-card>
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="Air Quality" force-render>
+        <a-card v-if="name != null">
+          <a-space align="baseline" :size="40">
+            <a-typography-title>{{ name }}</a-typography-title>
+            <a-typography-title :level="3"> {{ temp }}°C </a-typography-title>
+          </a-space>
+          <div>
+            <a-space :size="30">
+              <a-typography-text>Pressure:{{ pressure }}hPa </a-typography-text>
+              <a-typography-text>
+                Visibility:{{ visibility }}m
+              </a-typography-text>
+            </a-space>
+          </div>
+          <div>
+            <a-space :size="30">
+              <a-typography-text>Wind: {{ windSpeed }}m/s </a-typography-text>
+              <a-typography-text>Humidity: {{ humidity }}% </a-typography-text>
+            </a-space>
+          </div>
+        </a-card>
+        <a-card v-else>
+          <a-typography-title :level="4">
+            Write a name of the country you want to check air quality of in the
+            search bar
+          </a-typography-title>
+        </a-card>
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="Other">
+        <a-card>
+          <a-typography-title :level="4">
+            {{ air }}
+          </a-typography-title>
+        </a-card>
+      </a-tab-pane>
+    </a-tabs>
   </a-space>
 </template>
 
@@ -43,12 +85,17 @@ export default defineComponent({
   name: 'SearchBar',
   data: () => ({
     searchLocation: '',
-    info: null,
-    info2: null,
-    info3: null,
-    info4: null,
-    info5: null,
-    info6: null,
+    name: null,
+    temp: null,
+    windSpeed: null,
+    humidity: null,
+    pressure: null,
+    visibility: null,
+    country: null,
+    lat: null,
+    lon: null,
+    air: null,
+    activeKey: '1',
   }),
   methods: {
     getLocation() {
@@ -59,12 +106,28 @@ export default defineComponent({
             '&units=metric&limit=5&appid=67cd127ea4ab6b435f50093b78e4d4d6'
         )
         .then((response) => {
-          this.info = response.data.name;
-          this.info2 = response.data.main.temp;
-          this.info3 = response.data.wind.speed;
-          this.info4 = response.data.main.humidity;
-          this.info5 = response.data.main.pressure;
-          this.info6 = response.data.visibility;
+          this.name = response.data.name;
+          this.temp = response.data.main.temp;
+          this.windSpeed = response.data.wind.speed;
+          this.humidity = response.data.main.humidity;
+          this.pressure = response.data.main.pressure;
+          this.visibility = response.data.visibility;
+          this.country = response.data.sys.country;
+          this.lat = response.data.coord.lat;
+          this.lon = response.data.coord.lon;
+        });
+    },
+    getAirPollution() {
+      axios
+        .get(
+          'http://api.openweathermap.org/data/2.5/air_pollution?lat=' +
+            this.lat +
+            '&lon=' +
+            this.lon +
+            '&appid=67cd127ea4ab6b435f50093b78e4d4d6'
+        )
+        .then((response) => {
+          this.air = response.data.list[1];
         });
     },
   },
